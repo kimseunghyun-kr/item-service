1233835f4f3bcfd9f94f8574cacea13753aeb565
kimseunghyun-kr <incongnito12@gmail.com>
2023-06-08 23:49:37 +0900

    item manager page done thymeleaf from inflearn - kimyonghan , delete portion of code done originally by me

--- src/main/java/hello/itemservice/web/item/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/BasicItemController.java
@@ -4,18 +4,18 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
+@Slf4j
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
@@ -27,6 +27,7 @@ public class BasicItemController {
         return "basic/items";
     }
 
+////    requestParam only works in following scenarios ?id=abc
 //    @GetMapping("/{itemId}")
 //    public String Item(Model model, @RequestParam Long itemId){
 //        Item item = itemRepository.findById(itemId);
@@ -41,6 +42,93 @@ public String item(@PathVariable Long itemId, Model model) {
     model.addAttribute("item", item);
     return "basic/item";
 }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addFormV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+
+        Item item = new Item(itemName, price/1.0, quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addFormV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //automatically added by modelattribute. able to skip
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute skipped
+     * model.addAttribute(item) automatically added
+     */
+//    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getItemId();
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getItemId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+//    send data of existing item to thymeleaf view for editForm view
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping ("/{itemID}/edit")
+    public String edit(@PathVariable Long itemID, @ModelAttribute Item item) {
+        itemRepository.update(itemID, item);
+        return "basic/item";
+    }
+
+
+    @GetMapping("/{itemId}/delete")
+    public String deleteForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/deleteForm";
+    }
+
+    @PostMapping ("/{itemID}/delete")
+    public String delete(@PathVariable Long itemID, Model model) {
+        itemRepository.delete(itemID);
+        String ret = this.items(model);
+        log.info(ret);
+        return "redirect:/" + ret;
+    }
+
 
     /**
      * 테스트용 데이터 추가
